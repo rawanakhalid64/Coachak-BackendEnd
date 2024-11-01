b34819bae83dacf7db8ff2d5ec5846db32ea86ba
unknown <rawankhalid557@gmail.com>
2024-11-01 13:22:45 +0200

    coachak-backend repo

---
+++ utils/mailSender.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+const mailSender = async (email, title, body) => {
+  try {
+    // create Transport to send email
+    console.log(process.env.MAIL_HOST);
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+
+      auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
+    });
+    const info = await transporter.sendMail({
+      from: "coachak.com",
+      to: email,
+      subject: title,
+      html: body,
+    });
+    console.log("Email info: ", info);
+    return info;
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+module.exports = mailSender;
