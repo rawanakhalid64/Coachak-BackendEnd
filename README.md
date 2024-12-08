2a870a669f8bb2ddd9efdc757ed55cd15a4e3ac7
bukhaled2002 <Bakhaled.310@gmail.com>
2024-12-08 14:00:45 +0200

    add changes

--- README.md
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-To start application
-cd ./backend
-docker-compose up
-=======
-
->>>>>>> 87c89363440156aea40690d4230ba6dc9a91463e
--- utils/helper.js
+++
@@ -1,9 +0,0 @@
-exports.filterObj = (obj, ...filterItems) => {
-  let newObj = {};
-  Object.keys(obj).forEach((el) => {
-    if (!filterItems.includes(el)) {
-      newObj[el] = obj[el];
-    }
-  });
-  return newObj;
-};
