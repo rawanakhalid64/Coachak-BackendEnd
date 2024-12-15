1badb6078060c4b6228eba4cc7fb26f2de3c5ee7
bukhaled2002 <Bakhaled.310@gmail.com>
2024-12-15 17:30:41 +0200

    solve cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
