f1540c924657041f77c99801b998acdf999d9fb9
bukhaled2002 <Bakhaled.310@gmail.com>
2025-06-09 06:17:27 +0300

    get all exercises endpoint

--- routes/exerciseRoute.js
+++ routes/exerciseRoute.js
@@ -4,11 +4,12 @@ const { restrictTo } = require("../utils/helper");
 const {
   createExercise,
   editExercise,
+  getAllExercises,
 } = require("../controllers/exerciseController");
 const route = express.Router();
 
 route.use(protect);
-route.route("/").get();
+route.route("/").get(getAllExercises);
 route.route("/").post(restrictTo("trainer"), createExercise);
 route.route("/:id").patch(restrictTo("trainer"), editExercise);
 
